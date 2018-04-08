814e5bcf4b7f9922cfa994ce38a8f9a1f9554bda
artemholovnia <holovnia.artem@gmail.com>
2018-04-08 12:57:07 +0300

    dev

--- static/main.js
+++ static/main.js
@@ -34,6 +34,44 @@
             },
             error:console.log('error')
         })
-    }
+    };
+
+
+    //*сортировка*//
+    function get_sort(sort_by, unique_identificator , token){
+        $.ajax({
+            url: '/shop/getSortQuery/',
+            method: 'POST',
+            data:{
+                'csrfmiddlewaretoken':token,
+                'sort_by':sort_by,
+                'unique_identificator':unique_identificator,
+            },
+            success:function (data) {
+                hide_tags(['.card']);
+                for (one_product=0; one_product<data.data.length; one_product++){
+                    $one_product_card = $('#product-cards').append($('#hidden-template').html());
+                    generate_one_product_card(data.data[one_product]);
+                };
+                hide_tags(tags_list);
+                hover_product_card();
+            },
+            error:console.log('error')
+        })
+    };
+
+    //*генерация карточки*//
+        function generate_one_product_card(product_query) {
+            $this_card = $('.card').last();
+            $this_card.attr('id', product_query.article_number);
+            $this_card.find('.card-title').text(product_query.title);
+            $this_card.find('.card-text').text(product_query.description);
+            $this_card.find('.card-price').text(product_query.price + ' PLN');
+            $this_card.find('.get-all-images').attr('data-unique_identificator', product_query.unique_identificator);
+            $this_card.find('.btn-info').attr('onclick', 'window.location.href=' + "'/shop/product_detail/product=" + product_query.article_number + "'");
+            $this_card.find('.productImage').attr('src', product_query.image);
+        }
+
+
 
 
