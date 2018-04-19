5a8fa86de6b687670100b12852720e7bb84dc356
Артем Головня <artem@MacBook-Pro-Artem.local>
2018-04-19 19:29:41 +0200

    dev

--- static/main.js
+++ static/main.js
@@ -45,11 +45,14 @@
                 'searched_text': searched_text,
             },
             success:function (data) {
+                var template = $.template('#searchTemplate');
                 to_paste = $('#searched_query');
                 to_paste.find('li').remove();
                 to_paste.hide();
                 for (product=0; product<data.products.length; product++){
-                    to_paste.append('<li><a href="/shop/product_detail/product=' + data.products[product].article_number + '">' + data.products[product].title + ' ' + data.products[product].article_number +  '</a></li>')
+                    render_template = {'image_url':'', 'product_title':data.products[product].title, 'article_number':data.products[product].article_number};
+                    var html = template.render(render_template);
+                    console.log(html);
                 };
                 to_paste.show();
             },
