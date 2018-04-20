d1c969499275cb89f11f8cb129e7bfc32462539b
Артем Головня <artem@MacBook-Pro-Artem.local>
2018-04-20 12:00:50 +0200

    dev

--- static/main.js
+++ static/main.js
@@ -52,7 +52,7 @@
                 for (product=0; product<data.products.length; product++){
                     render_template = {'image_url':'', 'product_title':data.products[product].title, 'article_number':data.products[product].article_number};
                     var html = Mustache.render(template, render_template, castom_tags);
-                    console.log(html);
+                    console.log('rendered html ', html);
                 };
                 to_paste.show();
             },
