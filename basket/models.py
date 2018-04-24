3ad5a1996fe71c2fd5704ca774cd1f68a4ab06a4
artemholovnia <holovnia.artem@gmail.com>
2018-04-24 17:28:38 +0200

    dev

---
+++ basket/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from ikea_parser.create_identificator import create_identificator
+from ikea_parser.models import Product
+
+# Create your models here.
+class Order(models.Model):
+    unique_identificator = models.CharField(max_length=16, default=create_identificator(16), blank=False, null=False)
+    order_price = models.FloatField(default=0.0, blank=True, null=True)
+    status = models.SmallIntegerField(default=0, blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True, blank=False, null=False)
+    updated = models.DateTimeField(auto_now=True, blank=False, null=False)
+
+
+class ProductInOrder(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    count = models.SmallIntegerField(default=0, blank=False, null=True)
+    price = models.FloatField(default=0.0, blank=False, null=True)
--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils.timezone import datetime
+from ikea_parser.create_identificator import create_identificator
 
 # Create your models here.
+
+class Coef(models.Model):
+    coef = models.FloatField(default=0.0, blank=False, null=False, verbose_name='Коефициент продажи')
+    updated = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
+
+    def __str__(self):
+        return '%f updated in %s' % (self.coef, self.updated)
+
