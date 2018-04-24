64e4fa47f1030ea7b484bf06c4513131b40963ac
artemholovnia <holovnia.artem@gmail.com>
2018-04-24 18:40:37 +0200

    dev

--- basket/models.py
+++ basket/models.py
@@ -7,6 +7,7 @@ class Order(models.Model):
     unique_identificator = models.CharField(max_length=16, default=create_identificator(16), blank=False, null=False)
     order_price = models.FloatField(default=0.0, blank=True, null=True)
     status = models.SmallIntegerField(default=0, blank=True, null=True)
+    session_key = models.CharField()
     created = models.DateTimeField(auto_now_add=True, blank=False, null=False)
     updated = models.DateTimeField(auto_now=True, blank=False, null=False)
 
