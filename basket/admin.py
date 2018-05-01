3e437b0ed5dfffb697a7859e070c6773bc03049b
artemholovnia <holovnia.artem@gmail.com>
2018-05-01 13:02:22 +0200

    dev

--- basket/admin.py
+++ basket/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+class ProductInOrderInline(admin.StackedInline):
+    model = ProductInOrder
+    exclude = ('order', 'product')
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [ProductInOrderInline]
+    extra = 1
+
+admin.site.register(OrderAdmin)
