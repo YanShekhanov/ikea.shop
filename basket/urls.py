943bd572f1c9cd3c3ea27abcd9f166a0a33aefcc
artemholovnia <holovnia.artem@gmail.com>
2018-05-01 12:29:48 +0200

    dev

--- basket/urls.py
+++ basket/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     url(r'^add_to_basket/$', add_to_basket, name='add_to_basket'),
     url(r'^$', ShowBasket.as_view(), name='show_basket'),
+    url(r'^change_product/$', change_product, name='change_product'),
 ]
