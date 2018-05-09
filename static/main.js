f6dfa3600388df38db7da8d2b4507dcd46ec9f51
artemholovnia <holovnia.artem@gmail.com>
2018-05-09 11:06:01 +0200

    dev

--- static/main.js
+++ static/main.js
@@ -164,8 +164,9 @@
                     'csrfmiddlewaretoken':token
                 },
                 success:function (data) {
+                    console.log(data);
                     $('#product_count').html(data.order.product_count);
-                    $('#price').html(data.order.price);
+                    $('#price').html(data.order.order_price);
                 },
                 error:function (data) {
                     console.log(data);
