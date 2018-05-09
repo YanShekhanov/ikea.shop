990c7d83c197dd67b9f2a6c31b32ef39a622e992
artemholovnia <holovnia.artem@gmail.com>
2018-05-09 13:42:30 +0200

    dev

--- admin_panel/views.py
+++ admin_panel/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import TemplateView, ListView
 from basket.models import *
-from django.shortcut import render, redirect, reverse, JsonResponse
+from django.shortcuts import render, redirect, reverse, Http404
+from django.http import JsonResponse
 
 class DisplayOrders(ListView):
     template_name = 'admin_panel/orders.html'
@@ -8,7 +9,7 @@ class DisplayOrders(ListView):
     model = Order
 
     def get_queryset(self):
-        self.queryset = self.model._default_manager.exclude(status=0)
+        self.queryset = self.model._default_manager.exclude(status=0).order_by('-created')
         return self.queryset
 
     def get_context_data(self, **kwargs):
@@ -27,6 +28,24 @@ class DisplayOrders(ListView):
         return context
 
 def order_detail(request):
+    response_dict = {}
     if request.method == 'POST' and request.is_ajax():
-        print(request.POST['unique_identificator'])
-        return JsonResponse({'data':True})
+        products_list = []
+        order = Order.objects.get(unique_identificator=request.POST['unique_identificator'])
+        products_in_order = ProductInOrder.objects.filter(order=order).order_by('-created')
+        for product in products_in_order:
+            image = Product.objects.filter(product=product.product, size=250).first()
+            one_product_dict = {
+                'title':product.product.title,
+                'article_number':product.product.article_number,
+                'price_per_one':product.product.price,
+                'price':product.price,
+                'count':product.count,
+                'image':image.image.url,
+            }
+            products_list.append(one_product_dict)
+        response_dict = {'success':True, 'products':products_list}
+        return JsonResponse(response_dict)
+    else:
+        response_dict['methodError'] = 'Bad request'
+        raise Http404(response_dict)
