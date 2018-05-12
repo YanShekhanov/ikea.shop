b00f60a93429c049bea74293ba6fd437d4966b1a
artemholovnia <holovnia.artem@gmail.com>
2018-05-12 15:07:25 +0200

    dev

--- shop/urls.py
+++ shop/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     url(r'^product_detail/product=(?P<article_number>\w+)/$', ProductDetail.as_view(), name='productDetail'),
     url(r'^query=(?P<category_identificator>\w+)/$', GetOneCategoryProducts.as_view(), name='getOneCategoryProducts'),
     url(r'rooms/room_place=(?P<unique_identificator>\w+)/$', RoomPlaceDetail.as_view(), name='roomPlaceDetail'),
-    url(r'rooms/example=(?P<unique_identificator>)', ExampleDetail.as_view(), name='exampleDetail'),
+    url(r'rooms/example=(?P<unique_identificator>\w+)/$', ExampleDetail.as_view(), name='exampleDetail'),
     #url(r'rooms/room_detail/room=(?P<unique_identificator>\w+)/$', RoomExampleDetail.as_view(), name='roomExampleDetail'),
 
     #ajax
