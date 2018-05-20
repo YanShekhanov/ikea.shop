afb203198ba26407467d92692d9592130db38577
artemholovnia <holovnia.artem@gmail.com>
2018-05-20 22:58:56 +0300

    dev

--- admin_panel/urls.py
+++ admin_panel/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     url(r'^auth/$', AdminAuth.as_view(), name='adminAuth'),
     url(r'^check_auth/$', check_auth, name='check_auth'),
     url(r'^orders/$', DisplayOrders.as_view(), name='display_orders'),
+    url(r'^edit_product/product=(?P<article_number>\w+)/$', UpdateProduct.as_view(), name='edit_product'),
     url(r'^order_detail/$', order_detail, name='order_detail'), #ajax
     url(r'^change_order_status/$', change_order_status, name='change_order_status'), #ajax
 ]
