0ef17fb74c4dbca3c1337fe79f5696bdfab2f2e9
artemholovnia <holovnia.artem@gmail.com>
2018-05-21 17:45:24 +0300

    dev

--- admin_panel/urls.py
+++ admin_panel/urls.py
@@ -3,7 +3,6 @@ from .views import *
 
 urlpatterns = [
     url(r'^auth/$', AdminAuth.as_view(), name='adminAuth'),
-    url(r'^check_auth/$', check_auth, name='check_auth'),
     url(r'^orders/$', DisplayOrders.as_view(), name='display_orders'),
     url(r'^edit_product/product=(?P<article_number>\w+)/$', UpdateProduct.as_view(), name='edit_product'),
     url(r'^order_detail/$', order_detail, name='order_detail'), #ajax
