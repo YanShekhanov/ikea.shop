947b945b1cb934058f56e12c1e07090d13456eb7
artemholovnia <holovnia.artem@gmail.com>
2018-05-26 23:52:19 +0300

    dev

--- admin_panel/models.py
+++ admin_panel/models.py
@@ -4,7 +4,7 @@ from django.utils.timezone import datetime
 # Create your models here.
 
 class Process(models.Model):
-    process_name = models.CharField(blank=False, null=False, default=None, verbose_name='Название процесса')
+    process_name = models.CharField(max_length=32,blank=False, null=False, default=None, verbose_name='Название процесса')
     time_start = models.DateTimeField(blank=False, null=False, default=datetime.now(), verbose_name='Начало')
     time_end = models.DateTimeField(blank=True, null=False, default=datetime.now(), verbose_name='Конец')
     executable = models.BooleanField(blank=True, null=False, default=True, verbose_name='Выполняется')
